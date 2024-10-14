d50f1b346dd6af7f684aa23387c057120334af3d
yuma <yuma.fuu05@gmail.com>
2024-10-14 14:54:23 +0900

    [update] tmux

--- karabiner/app.ts
+++ karabiner/app.ts
@@ -1,4 +1,6 @@
 export const Vivaldi = "com.vivaldi.Vivaldi"
+export const Discord = "com.hnc.Discord"
 export const Slack = "com.tinyspeck.slackmacgap"
 export const ReflectApp = "app.reflect.ReflectDesktop"
 export const Spark = "com.readdle.SparkDesktop"
+export const Wezterm = "com.github.wez.wezterm"
