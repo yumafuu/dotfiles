b8182c60029a98885f7321a08e4a44484a000555
yuma <yuma.fuu05@gmail.com>
2024-10-24 17:56:18 +0900

    [update] nvim

--- nvim/lua/lazy-nvim.lua
+++ nvim/lua/lazy-nvim.lua
@@ -1,15 +1,15 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--single-branch",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 vim.opt.runtimepath:prepend(lazypath)
 vim.g.mapleader = "'"
 
-require('lazy').setup(require('plugins'))
+require("lazy").setup(require("plugins"))
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,29 +1,29 @@
 local hl = vim.api.nvim_set_hl
 
-hl(0, 'NormalFloat', { bg=None })
-hl(0, 'FloatBorder', { bg=None, fg='#444444' })
-hl(0, 'ActiveWindow', { bg=None })
-hl(0, 'InactiveWindow', { bg=None })
-hl(0, 'LineNr', { fg='#3A3A3A' })
-hl(0, 'NotifyBackground', { bg=None })
-hl(0, "TelescopeNormal", { bg=None })
-hl(0, "HlSearchNear", { bg='#444444', fg='#7CB0FF' })
-hl(0, "HlSearchLens", { bg=None, fg='#7CB0FF' })
-hl(0, "HlSearchLensNear", { bg='None', fg='#7BAFDA' })
+hl(0, "NormalFloat", { bg = None })
+hl(0, "FloatBorder", { bg = None, fg = "#444444" })
+hl(0, "ActiveWindow", { bg = None })
+hl(0, "InactiveWindow", { bg = None })
+hl(0, "LineNr", { fg = "#3A3A3A" })
+hl(0, "NotifyBackground", { bg = None })
+hl(0, "TelescopeNormal", { bg = None })
+hl(0, "HlSearchNear", { bg = "#444444", fg = "#7CB0FF" })
+hl(0, "HlSearchLens", { bg = None, fg = "#7CB0FF" })
+hl(0, "HlSearchLensNear", { bg = "None", fg = "#7BAFDA" })
 
 vim.cmd("set winhighlight=Normal:MyNormal,NormalNC:MyNormalNC")
 
 local o = vim.opt
 
 o.fillchars = {
-  vert = '│',
-  vertleft  = '┤',
-  vertright = '├',
-  verthoriz = '┼',
-  horiz = '─',
-  horizup = '┴',
-  horizdown = '┬',
-  eob = ' ',
+	vert = "│",
+	vertleft = "┤",
+	vertright = "├",
+	verthoriz = "┼",
+	horiz = "─",
+	horizup = "┴",
+	horizdown = "┬",
+	eob = " ",
 }
 o.termguicolors = true
 o.confirm = true
@@ -41,7 +41,7 @@ o.expandtab = true
 o.shiftwidth = 2
 o.tabstop = 2
 o.list = true
-o.listchars = {tab='▸-', trail='·', extends='»', precedes='«', nbsp='%'}
+o.listchars = { tab = "▸-", trail = "·", extends = "»", precedes = "«", nbsp = "%" }
 o.fileformats = "unix,dos,mac"
 o.fileencodings = "utf-8,sjis"
 o.hlsearch = true
@@ -54,11 +54,11 @@ o.secure = true
 o.nu = true
 o.wildmenu = true
 o.wildmode = "full"
-o.encoding = 'UTF-8'
+o.encoding = "UTF-8"
 o.splitbelow = true
 o.splitright = true
 o.foldtext = "FoldCCtext()"
-o.clipboard:append{'unnamedplus'}
+o.clipboard:append({ "unnamedplus" })
 o.laststatus = 3
 o.cmdheight = 1
 
@@ -68,60 +68,60 @@ o.cmdheight = 1
 --   callback = function() print('insert enter') end
 -- })
 
-if vim.fn.has('linux') == 1 then
-  vim.g.clipboard = {
-    name = 'lemonade',
-    copy = {
-      ['+'] = 'lemonade copy',
-      ['*'] = 'lemonade copy',
-    },
-    paste = {
-      ['+'] = 'lemonade paste',
-      ['*'] = 'lemonade paste',
-    },
-    cache_enabled = 0,
-  }
+if vim.fn.has("linux") == 1 then
+	vim.g.clipboard = {
+		name = "lemonade",
+		copy = {
+			["+"] = "lemonade copy",
+			["*"] = "lemonade copy",
+		},
+		paste = {
+			["+"] = "lemonade paste",
+			["*"] = "lemonade paste",
+		},
+		cache_enabled = 0,
+	}
 end
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*" },
-  command = [[%s/\s\+$//e]],
+	pattern = { "*" },
+	command = [[%s/\s\+$//e]],
 })
 
 local map = vim.api.nvim_set_keymap
 maps = {
-  n = {
-    -- [';']          = ':',
-    -- ['te']         = ':execute ":e" expand("%:h")<CR>',
-    ['te']         = '<cmd>execute ":tabe" expand("%:h")<CR>',
-    ['<C-[><C-[>'] = '<cmd>noh<CR><Esc>',
-    ['C-l']        = '<cmd>noh<CR>',
-    ["<C-[><C-[>"] = '<cmd>noh<CR>',
-    ["<ECS><ECS>"] = '<cmd>noh<CR>',
-    ['j']          = 'gj',
-    ['k']          = 'gk',
-    ['R']          = '<Plug>(operator-replace)',
-    ['go']         = '<Plug>(openbrowser-smart-search)',
-    ['<Leader>c'] = '<plug>(operator-camelize-toggle)',
-    ['<C-n>'] = '<cmd>tabnext<CR>',
-    ['<C-p>'] = '<cmd>tabprevious<CR>',
-    ['tt'] = '<cmd>terminal<CR>',
-    ['tv'] = '<cmd>vsplit | terminal<CR>',
-    ['ts'] = '<cmd>split | terminal<CR>',
-  },
-  i = {
-    ['<C-h>'] = '<Left>',
-    ['<C-l>'] = '<Right>',
-  },
-  v = {
-    ['go'] = '<Plug>(openbrowser-smart-search)',
-  },
+	n = {
+		-- [';']          = ':',
+		-- ['te']         = ':execute ":e" expand("%:h")<CR>',
+		["te"] = '<cmd>execute ":tabe" expand("%:h")<CR>',
+		["<C-[><C-[>"] = "<cmd>noh<CR><Esc>",
+		["C-l"] = "<cmd>noh<CR>",
+		["<C-[><C-[>"] = "<cmd>noh<CR>",
+		["<ECS><ECS>"] = "<cmd>noh<CR>",
+		["j"] = "gj",
+		["k"] = "gk",
+		["R"] = "<Plug>(operator-replace)",
+		["go"] = "<Plug>(openbrowser-smart-search)",
+		["<Leader>c"] = "<plug>(operator-camelize-toggle)",
+		["<C-n>"] = "<cmd>tabnext<CR>",
+		["<C-p>"] = "<cmd>tabprevious<CR>",
+		["tt"] = "<cmd>terminal<CR>",
+		["tv"] = "<cmd>vsplit | terminal<CR>",
+		["ts"] = "<cmd>split | terminal<CR>",
+	},
+	i = {
+		["<C-h>"] = "<Left>",
+		["<C-l>"] = "<Right>",
+	},
+	v = {
+		["go"] = "<Plug>(openbrowser-smart-search)",
+	},
 }
 
 for mode, _maps in pairs(maps) do
-  for k, v in pairs(_maps) do
-    vim.keymap.set(mode, k, v, { noremap = true, silent = true })
-  end
+	for k, v in pairs(_maps) do
+		vim.keymap.set(mode, k, v, { noremap = true, silent = true })
+	end
 end
 
 vim.cmd([[
@@ -143,7 +143,7 @@ tnoremap <C-W>l       <cmd>wincmd l<cr>
 command R Dispatch
 ]])
 
-vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal(), {noremap = true})
-vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal(), {noremap = true})
-vim.keymap.set("v", "<C-a>", require("dial.map").inc_visual(), {noremap = true})
-vim.keymap.set("v", "<C-x>", require("dial.map").dec_visual(), {noremap = true})
+vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
+vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
+vim.keymap.set("v", "<C-a>", require("dial.map").inc_visual(), { noremap = true })
+vim.keymap.set("v", "<C-x>", require("dial.map").dec_visual(), { noremap = true })
