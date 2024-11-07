8f2bafcd03080d35a44f528f1c9d4e8e817b093e
yuma <yuma.fuu05@gmail.com>
2024-11-07 15:36:27 +0900

    [update]

---
+++ zsh/alias.zsh
@@ -0,0 +1,37 @@
+alias q=exit
+alias vz='vim ~/.zshrc'
+alias v='vim .'
+alias ez='exec zsh'
+alias vv='vim ~/.config/nvim/init.lua'
+alias ls='exa -a'
+alias tree='exa --tree'
+alias ql='qlmanage -p "$@" >& /dev/null'
+alias imgcat='img2sixel'
+alias x='bun x'
+alias ag="ag -u"
+alias ls='exa -g --time-style=long-iso -a'
+alias ll="ls -la"
+alias tree="exa -T -a -I .git --git-ignore"
+alias :q='exit'
+alias q='exit'
+alias mv='mv -i'
+alias cp='cp -i'
+alias vz='vim ~/.zshrc '
+alias ve='vim ~/.zshenv '
+alias ez='exec zsh'
+alias se='source ~/.zshenv'
+alias vz="nvim ~/.zshrc"
+alias vv="nvim ~/.config/nvim/"
+alias ..="cd .."
+alias ...="cd ../../.."
+alias spotify="spt"
+
+alias b='bundle -j4'
+alias be="bundle exec"
+
+alias dk='docker'
+alias dkc="docker compose"
+alias dck="docker compose"
+alias tf='terraform'
+alias ql='qlmanage -p "$@" >& /dev/null'
+
