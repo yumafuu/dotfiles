12fb29153fea5c2dac1ff58febc43f89a854f429
yuma ishikawa <yuma.ishikawa@yumanoMacBook-Pro.local>
2024-11-09 14:23:15 +0900

    [update]

--- zsh/zsh-config.zsh
+++ zsh/zsh-config.zsh
@@ -1,9 +1,8 @@
 # zstyle
 zstyle ':completion:*' menu select
-zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' list-colors "${(@s.:.)LS_COLORS}"
 zstyle ':completion:*' ignore-parents parent pwd ..
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
 
 # options
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
