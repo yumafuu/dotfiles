47a1ad34ed10a1da3efd4f3875e4318daaf1d6ae
Yuma Ishikawa <yuma.fuu05@gmail.com>
2024-12-03 08:13:43 +0900

    [update]

--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -1,1 +1,11 @@
+# password
 alias pswd='ruby -rsecurerandom -e "puts SecureRandom.alphanumeric"|xargs echo -n|pbcopy'
+
+# jira
+function _openJiraFromCurrentBranch() {
+  # KWS-[0-9]+
+  ticket=$(git rev-parse --abbrev-ref HEAD | grep -o 'KWS-[0-9]\+')
+
+  open "https://knowledgework.atlassian.net/browse/$ticket"
+}
+alias jira=_openJiraFromCurrentBranch
