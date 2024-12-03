ce24d3068602734448eb3ed7ac47d514e1730c05
Yuma Ishikawa <yuma.fuu05@gmail.com>
2024-12-03 10:01:52 +0900

    [tmp]
    Co-authored-by: [update]

--- zsh/ghq-fzf.zsh
+++ zsh/ghq-fzf.zsh
@@ -1,10 +1,20 @@
 function _fzf_cd_ghq() {
   FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --reverse --height=50%"
-  local root="$(ghq root)"
-  local list=$(ghq list)
+  local ghqroot="$(ghq root)"
+  local ghqlist=$(ghq list)
 
-  list+="\n../dotfiles"
-  list+="\n../Downloads"
+  IFS=$'\n'
+  local list=("${(@f)$(ghq list)}")
+  # add [ghq] previx to list
+  for s in $ghqlist; do
+    echo $s
+    echo ==========
+    list+="[ghq] $s\n"
+  done
+
+
+  list+="\n[custom] $HOME/Downloads"
+  list+="\n[custom] $HOME/dotfiles"
 
   local repo="$(echo "$list" | fzf --preview="exa --tree -L 2 --color=always ${root}/{1}")"
   local dir="${root}/${repo}"
